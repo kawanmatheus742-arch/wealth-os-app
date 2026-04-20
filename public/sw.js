986588925b511ae3d08d564f516e5438e5240574
kawanmatheus742-arch <kawanmatheus742@gmail.com>
2026-04-19 21:38:11 -0300

    pwa pronto

--- public/sw.js
+++ public/sw.js
@@ -1,29 +1,9 @@
-const CACHE_NAME = "wealth-os-v1";
-
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(["./", "./index.html", "./manifest.json"]))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
-        keys.map((key) => {
-          if (key !== CACHE_NAME) return caches.delete(key);
-        })
-      )
-    )
-  );
+  event.waitUntil(self.clients.claim());
 });
 
-self.addEventListener("fetch", (event) => {
-  if (event.request.method !== "GET") return;
-
-  event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      return cachedResponse || fetch(event.request);
-    })
-  );
-});
+self.addEventListener("fetch", () => {});
